492ca1e11116c6b3464f98743f20735434ed6c6a
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-23 14:02:42 -0500

    establecer y establecer el valor del formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 700,
+  inStorage: 6,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0, [], []),
+  //   inStorage: new FormControl(0, [], []),
+  // });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // establecer valor inicial al formulario
+    this.myForm.reset(rtx5090);
+  }
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  onSave(): void {
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -5,13 +5,14 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     DynamicPageComponent,
     SwitchesPageComponent,
-    BasicPageComponent
+    BasicPageComponent,
   ],
-  imports: [CommonModule, ReactiveRoutingModule],
+  imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
 export class ReactiveModule {}
