1cce02e269bcb374562b39da053b665b0a6c1dc2
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-23 12:31:03 -0500

    establecimiento de rutas y formularios templates

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,20 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
-import { BasicPagesComponent } from './pages/basic-pages/basic-pages.component';
-import { SwitchesPagesComponent } from './pages/switches-pages/switches-pages.component';
-import { DynamicPagesComponent } from './pages/dynamic-pages/dynamic-pages.component';
-
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 
 @NgModule({
   declarations: [
-    BasicPagesComponent,
-    SwitchesPagesComponent,
-    DynamicPagesComponent
+    DynamicPageComponent,
+    SwitchesPageComponent,
+    BasicPageComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule
-  ]
+  imports: [CommonModule, ReactiveRoutingModule],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}
