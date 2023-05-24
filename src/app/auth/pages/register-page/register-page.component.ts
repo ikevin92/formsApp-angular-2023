0c6edc0b95261fbbe7cf2210e14cfc7ee6d39230
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-23 23:30:00 -0500

    se adicionan propiedades al formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: Obtener validacion de un servicio
+    // return (
+    //   this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    // );
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
