838bf9ab2ce04bc89dc983cda2a6c62354a0778b
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-23 19:45:19 -0500

    metodo de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -25,19 +25,46 @@ export class BasicPageComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    // establecer valor inicial al formulario
-    this.myForm.reset(rtx5090);
-  }
-
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
+  ngOnInit(): void {
+    // establecer valor inicial al formulario
+    // this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'El campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors[key].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
+  }
+
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
 
